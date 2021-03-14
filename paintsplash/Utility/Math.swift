0f35b7eb6da10f02f4484ce7ea9a3079b0be258b
Ho Pin Xian <hopinxian@gmail.com>
2021-03-14 21:04:10 +0800

    Add greatest common divisor functions

---
+++ paintsplash/Utility/Math.swift
@@ -0,0 +1,41 @@
+//
+//  Math.swift
+//  paintsplash
+//
+//  Created by Ho Pin Xian on 14/3/21.
+//
+
+/**
+ `Math` contains useful utility functions.
+ */
+struct Math {
+    /// Returns the greatest common divisor among the given array of integers.
+    /// The array of integers must not be empty.
+    static func getGCD(numbers: [Int]) -> Int {
+        assert(numbers.count != 0)
+        
+        var gcd = numbers[0]
+        for i in 1..<numbers.count {
+            gcd = getGCD(gcd, numbers[i])
+        }
+        
+        assert(gcd > 0)
+        return gcd
+    }
+    
+    /// Returns the greatest common divisor between the given positive integers.
+    static func getGCD(_ lhs: Int, _ rhs: Int) -> Int {
+        assert(lhs > 0 && rhs > 0)
+        
+        var big = max(lhs, rhs)
+        var small = min(lhs, rhs)
+        while (small != 0) {
+            let temp = big % small
+            big = small
+            small = temp
+        }
+        
+        assert(big > 0)
+        return big
+    }
+}
