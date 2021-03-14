3b6ccef1ea8ecf083c92b1e4537903652e564c93
Ho Pin Xian <hopinxian@gmail.com>
2021-03-14 21:16:17 +0800

    Update colour protocols

--- paintsplash/Color/Colorable.swift
+++ paintsplash/Color/Colorable.swift
@@ -5,6 +5,10 @@
 //  Created by Praveen Bala on 8/3/21.
 //
 
+/**
+ `Colorable` is a type that has a color.
+ */
 protocol Colorable {
-    var color: PaintColor { get set }
+    associatedtype ColorType: GameColor
+    var color: ColorType { get set }
 }
