2b354c0f6874ae4af06cb4c406e6728e06323c98
Ho Pin Xian <hopinxian@gmail.com>
2021-03-17 22:25:06 +0800

    Add back atlas and missing files

---
+++ paintsplash/Events/PlayerHasMovedEvent/PlayerHasMovedEvent.swift
@@ -0,0 +1,10 @@
+//
+//  PlayerHasMovedEvent.swift
+//  paintsplash
+//
+//  Created by Ho Pin Xian on 17/3/21.
+//
+
+struct PlayerHasMovedEvent: Event {
+    let location: Vector2D
+}
--- paintsplash/Weapon/AmmoDrop.swift
+++ paintsplash/Weapon/AmmoDrop.swift
@@ -5,7 +5,7 @@
 //  Created by Ho Pin Xian on 17/3/21.
 //
 
-protocol AmmoDrop: Ammo {
-    associatedtype AmmoType
-    func getAmmoObject() -> AmmoType
-}
+//protocol AmmoDrop: Ammo {
+//    associatedtype AmmoType
+//    func getAmmoObject() -> AmmoType
+//}
