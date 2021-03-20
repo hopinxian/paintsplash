874d5a712400a852385d62d654d89bdbe8c12147
boundtotheearth <farrellnah@gmail.com>
2021-03-20 15:11:47 +0800

    Small change

--- paintsplash/Health/HeartDisplay.swift
+++ paintsplash/Health/HeartDisplay.swift
@@ -11,19 +11,4 @@ class HeartDisplay: GameEntity {
         addComponent(RenderComponent(spriteName: "heart", defaultAnimation: nil, zPosition: Constants.ZPOSITION_UI_ELEMENT))
         super.init()
     }
-
-    override func spawn(gameManager: GameManager) {
-        gameManager.getRenderSystem().addRenderable(self)
-        super.spawn(gameManager: gameManager)
-    }
-
-    override func destroy(gameManager: GameManager) {
-        gameManager.getRenderSystem().removeRenderable(self)
-        super.destroy(gameManager: gameManager)
-    }
-
-    override func update(gameManager: GameManager) {
-        gameManager.getRenderSystem().updateRenderable(self)
-        super.spawn(gameManager: gameManager)
-    }
 }
