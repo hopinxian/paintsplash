867f559226a337e797109a255831991e7d7e1fa6
Cynthia <leewengyancynthia@gmail.com>
2021-03-26 13:37:00 +0800

    Fix paint rendering

--- paintsplash/Rendering/SKRenderSystem.swift
+++ paintsplash/Rendering/SKRenderSystem.swift
@@ -72,5 +72,35 @@ class SKRenderSystem: RenderSystem {
         }
     }
 
+    func addSubviewToEntity(_ entity: GameEntity, subviewInfo: RenderInfo) {
+        guard let node = nodeEntityMap[entity] else {
+            return
+        }
+
+        let subview = SKSpriteNode(imageNamed: subviewInfo.spriteName)
+
+        subview.position = SpaceConverter.modelToScreen(subviewInfo.position)
+        subview.size = SpaceConverter.modelToScreen(Vector2D(subviewInfo.width, subviewInfo.height))
+        subview.zPosition = CGFloat(node.zPosition + 1)
+        subview.color = subviewInfo.color.uiColor
+        subview.colorBlendFactor = CGFloat(subviewInfo.colorBlend)
+        subview.zRotation = CGFloat(subviewInfo.rotation)
+
+        if subviewInfo.cropInParent {
+            let cropNode = SKCropNode()
+
+            let maskNode = node.copy() as? SKSpriteNode
+            maskNode?.position = .zero
+
+            cropNode.maskNode = maskNode
+            cropNode.position = .zero
+            cropNode.zPosition = CGFloat(node.zPosition + 1)
+            cropNode.addChild(subview)
+
+            node.addChild(cropNode)
+        } else {
+            node.addChild(subview)
+        }
+    }
 
 }
