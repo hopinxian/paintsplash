f93972ebf94d3127b666b8a5f32e238771bccba0
pr4aveen <ajjuu98@gmail.com>
2021-04-18 01:25:54 +0800

    Remove unnecessary code

--- paintsplash/Events/Audio/AudioEventManager.swift
+++ paintsplash/Events/Audio/AudioEventManager.swift
@@ -9,15 +9,11 @@ class AudioEventManager: EventManager<AudioEvent> {
     let playMusicEvent = EventManager<PlayMusicEvent>()
     let playSoundEffectEvent = EventManager<PlaySoundEffectEvent>()
     let stopAudioEvent = EventManager<StopAudioEvent>()
-//    let stopMusicEvent = EventManager<StopMusicEvent>()
-//    let stopSoundEffectEvent = EventManager<StopSoundEffectEvent>()
 
     override func subscribe(listener: @escaping (AudioEvent) -> Void) {
         super.subscribe(listener: listener)
         playMusicEvent.subscribe(listener: listener)
         playSoundEffectEvent.subscribe(listener: listener)
         stopAudioEvent.subscribe(listener: listener)
-//        stopMusicEvent.subscribe(listener: listener)
-//        stopSoundEffectEvent.subscribe(listener: listener)
     }
 }
