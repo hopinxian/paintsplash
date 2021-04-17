a68247431dc32e502e16e4eed522fe1e0c06f216
Cynthia <leewengyancynthia@gmail.com>
2021-04-17 17:56:07 +0800

    Clean up connection handler

--- paintsplash/Networking/Firebase/FirebaseConnectionHandler.swift
+++ paintsplash/Networking/Firebase/FirebaseConnectionHandler.swift
@@ -3,7 +3,6 @@
 //  paintsplash
 //
 //  Created by Farrell Nah on 3/4/21.
-// swiftlint:disable function_parameter_count
 
 import Firebase
 
@@ -12,54 +11,81 @@ class FirebaseConnectionHandler: ConnectionHandler {
     var batchedOperations = [FirebaseOperation]()
     private var observers: [FirebaseObserver] = []
 
-    deinit {
-        print("Deinitializing FirebaseConnectionHandler")
-        // detach all observers
-        observers.forEach { observer in
-            observer.reference.removeObserver(withHandle: observer.handle)
-        }
-    }
-
     init() {
+        // Uncomment to use Firebase Emulator (development)
         self.firebase =
             Database.database(url: "http://localhost:9000/?ns=paintsplash-fe2d8")
-//        self.firebase = Database.database()
+
+        // Uncomment to use Firebase Real Time Database (production)
+        // self.firebase = Database.database()
     }
 
+    /// Writes the given data dictionary to the path in the Firebase Real Time database,
+    /// - Parameters:
+    ///   - shouldRemoveOnDisconnect: indicates if the written value at the path
+    ///    should be deleted from the database when disconnected
+    ///   - onComplete: callback invoked when data is successfully written
+    ///   - onError: callback invoked when error encountered
     func send<T: Codable>(
         to destination: String,
         data: T,
         mode: TransactionMode,
+        shouldRemoveOnDisconnect: Bool = true,
+        onComplete: (() -> Void)?,
+        onError: ((Error?) -> Void)?) {
+
+        let dataDict = data.dictionary
+        let operation = FirebaseOperation(path: destination, data: dataDict, onSuccess: onComplete, onError: onError)
+
+        switch mode {
+        case .single:
+            handleSingleWriteOperation(operation: operation, shouldRemoveOnDisconnect: shouldRemoveOnDisconnect)
+        case .batched:
+            handleBatchedWriteOperation(operation: operation)
+        }
+    }
+
+    /// Writes the given individual data valueto the path in the Firebase Real Time database
+    func sendSingleValue<T: Codable>(
+        to path: String,
+        data: T,
         shouldRemoveOnDisconnect: Bool,
         onComplete: (() -> Void)?,
         onError: ((Error?) -> Void)?
     ) {
-        let dataDict = data.dictionary
-
-        if mode == .single {
-            firebase.reference().child(destination).setValue(dataDict, withCompletionBlock: { error, ref in
-                if shouldRemoveOnDisconnect {
-                    ref.onDisconnectRemoveValue()
-                }
-                if error != nil {
-                    DispatchQueue.main.async {
-                        onError?(error)
-                    }
-                } else {
-                    DispatchQueue.main.async {
-                        onComplete?()
-                    }
-                }
-            })
-        } else if mode == .batched {
-            batchedOperations.append(
-                FirebaseOperation(
-                    path: destination, data: dataDict,
-                    onSuccess: onComplete, onError: onError)
-            )
-        }
+        let operation = FirebaseOperation(path: path, data: data, onSuccess: onComplete, onError: onError)
+        handleSingleWriteOperation(operation: operation, shouldRemoveOnDisconnect: shouldRemoveOnDisconnect)
     }
 
+    private func handleSingleWriteOperation(operation: FirebaseOperation, shouldRemoveOnDisconnect: Bool) {
+        let path = operation.path
+        let dataDict = operation.data
+        let onComplete = operation.onSuccess
+        let onError = operation.onError
+
+        firebase.reference().child(path).setValue(dataDict, withCompletionBlock: { error, ref in
+            if shouldRemoveOnDisconnect {
+                ref.onDisconnectRemoveValue()
+            }
+
+            guard error == nil else {
+                DispatchQueue.main.async {
+                    onError?(error)
+                }
+                return
+            }
+
+            DispatchQueue.main.async {
+                onComplete?()
+            }
+        })
+    }
+
+    private func handleBatchedWriteOperation(operation: FirebaseOperation) {
+        batchedOperations.append(operation)
+    }
+
+    /// Observes a dictionary value at the given path, and invokes the callback when the value changes
     func listen<T: Codable>(to source: String, callBack: @escaping (T?) -> Void) {
         let ref = firebase.reference().child(source)
         let observerHandle = ref.observe(DataEventType.value) { snapshot in
@@ -67,18 +93,32 @@ class FirebaseConnectionHandler: ConnectionHandler {
             let data = T(from: rawData)
             callBack(data)
         }
+
+        // Keep track of the listener and database reference to be
+        // removed later during deinitialization
         self.observers.append(FirebaseObserver(handle: observerHandle, reference: ref))
     }
 
-    func getData<T: Codable>(at path: String, block: @escaping (Error?, T?) -> Void) {
+    /// Observes a raw Codable value at the given path, and invokes the callback when the value changes
+    func listenToSingleValue<T: Codable>(to source: String, callBack: @escaping (T?) -> Void) {
+        let ref = firebase.reference().child(source)
+        let observerHandle = ref.observe(DataEventType.value) { snapshot in
+            let rawData = snapshot.value as? T
+            callBack(rawData)
+        }
+        self.observers.append(FirebaseObserver(handle: observerHandle, reference: ref))
+    }
+
+    /// Fetches data as a Codable from given path and invokes the given callback
+    func getData<T: Codable>(at path: String, callback: @escaping (Error?, T?) -> Void) {
         firebase.reference().child(path).getData(completion: { error, snapshot in
-            // TODO Error Handling
             let rawData = snapshot.value as? [String: AnyObject] ?? [:]
             let data = T(from: rawData)
-            block(error, data)
+            callback(error, data)
         })
     }
 
+    /// Fetches data as a dictionary from given path and invokes the given callback
     func getData(at path: String, block: @escaping (Error?, [String: Any]?) -> Void) {
         firebase.reference().child(path).getData(completion: { error, snapshot in
             let rawData = snapshot.value as? [String: AnyObject]
@@ -113,40 +153,22 @@ class FirebaseConnectionHandler: ConnectionHandler {
         })
     }
 
-    func removeData(at path: String, block: @escaping (Error?) -> Void) {
+    /// Erases data at the given path and invokes the given callback
+    func removeData(at path: String, callBack: @escaping (Error?) -> Void) {
         firebase.reference().child(path).removeValue(completionBlock: { error, _ in
-            block(error)
+            callBack(error)
         })
     }
 
-    func sendSingleValue<T: Codable>(
-        to path: String, data: T,
-        shouldRemoveOnDisconnect: Bool,
-        onComplete: (() -> Void)?,
-        onError: ((Error?) -> Void)?
-    ) {
-        firebase.reference().child(path).setValue(data, withCompletionBlock: { error, ref in
-            if shouldRemoveOnDisconnect {
-                ref.onDisconnectRemoveValue()
-            }
-            if error != nil {
-                DispatchQueue.main.async {
-                    onError?(error)
-                }
-            } else {
-                DispatchQueue.main.async {
-                    onComplete?()
-                }
-            }
-        })
-    }
-
-    func observeSingleValue<T: Codable>(to source: String, callBack: @escaping (T?) -> Void) {
-        let ref = firebase.reference().child(source)
-        let observerHandle = ref.observe(DataEventType.value) { snapshot in
-            let rawData = snapshot.value as? T
-            callBack(rawData)
+    /// Removes all callbacks on Firebase listeners
+    private func detachObservers() {
+        observers.forEach { observer in
+            observer.reference.removeObserver(withHandle: observer.handle)
         }
-        self.observers.append(FirebaseObserver(handle: observerHandle, reference: ref))
+    }
+
+    deinit {
+        print("Deinitializing FirebaseConnectionHandler")
+        detachObservers()
     }
 }
--- paintsplash/Networking/Protocols/ConnectionHandler.swift
+++ paintsplash/Networking/Protocols/ConnectionHandler.swift
@@ -14,20 +14,18 @@ protocol ConnectionHandler {
         onComplete: (() -> Void)?,
         onError: ((Error?) -> Void)?
     )
-    func listen<T: Codable>(to source: String, callBack: @escaping (T?) -> Void)
-    func getData<T: Codable>(at path: String, block: @escaping (Error?, T?) -> Void)
-    func getData(at path: String, block: @escaping (Error?, [String: Any]?) -> Void)
-    func removeData(at path: String, block: @escaping (Error?) -> Void)
-
-    // methods for individual value
     func sendSingleValue<T: Codable>(
-        to path: String, data: T,
+        to path: String,
+        data: T,
         shouldRemoveOnDisconnect: Bool,
         onComplete: (() -> Void)?,
         onError: ((Error?) -> Void)?
     )
-    func observeSingleValue<T: Codable>(
-        to source: String,
-        callBack: @escaping (T?) -> Void
-    )
+
+    func listen<T: Codable>(to source: String, callBack: @escaping (T?) -> Void)
+    func listenToSingleValue<T: Codable>(to source: String, callBack: @escaping (T?) -> Void)
+
+    func getData<T: Codable>(at path: String, callback: @escaping (Error?, T?) -> Void)
+    func getData(at path: String, block: @escaping (Error?, [String: Any]?) -> Void)
+    func removeData(at path: String, callBack: @escaping (Error?) -> Void)
 }
