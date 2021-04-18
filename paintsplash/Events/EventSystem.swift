7d85072ff0b0e7d8c708d5386868c7ef8625b93a
pr4aveen <ajjuu98@gmail.com>
2021-04-18 19:18:05 +0800

    Add reset to event system

--- paintsplash/Events/EventSystem.swift
+++ paintsplash/Events/EventSystem.swift
@@ -13,4 +13,14 @@ class EventSystem {
     static let scoreEvent = ScoreEventManager()
     static let audioEvent = AudioEventManager()
     static let gameStateEvents = GameStateEventManager()
+
+    static func reset() {
+        EventSystem.processedInputEvents.reset()
+        EventSystem.playerActionEvent.reset()
+        EventSystem.entityChangeEvents.reset()
+        EventSystem.canvasEvent.reset()
+        EventSystem.scoreEvent.reset()
+        EventSystem.audioEvent.reset()
+        EventSystem.gameStateEvents.reset()
+    }
 }
